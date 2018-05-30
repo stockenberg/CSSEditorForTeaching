45f2e9d37addaa9cccf6670fe93248bb4f4ac857
Marten Stockenberg <MStockenberg@googlemail.com>
2018-05-30 12:48:36 +0200

    new version

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <div id="app">
-    <br><br><br>
-    <h2 :class="classAttr">Example Font for teaching Purpose</h2>
-    <br><br><br>
-    <label for="">.{{classAttr}}</label>
-    <textarea name="" v-model="styles" id="" cols="30" rows="10"></textarea>
+  <div id="app" class="clearfix">
+    <div class="align-right">
+      <div class="viewport">
+        <h2 :class="classAttr">[h2] Example Font for teaching Purpose</h2>
+        <span>[span] This is a span</span>
+
+        <section>
+          <h3>[h3] this is a headline3, below ul[class=list][id=list]</h3>
+          <ul class="list">
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+          </ul>
+        </section>
+
+        <section>
+          <h3>[h3] this is a headline3, below div[class=container]</h3>
+
+          <div class="container"></div>
+          <div class="container"></div>
+        </section>
+
+        <figure>
+          <img src="./assets/logo.png" alt="This is an image" title="vuejs Logo">
+          <figcaption>figure>img+figcation</figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="align-left">
+      <textarea name="" contenteditable="true" v-model="styles" id="" cols="30" rows="10"></textarea>
+    </div>
   </div>
 </template>
 
@@ -25,13 +52,18 @@ export default {
   },
   data () {
     return {
+      classAttr: 'headline',
       styles:
-`{
+`.headline{
+  font-family: Arial, sans-serif;
+}
 
-color: red;
-
+.container{
+  height: 100px;
+  width: 100px;
+  background: red;
+  margin: 10px auto;
 }`,
-      classAttr: 'myClass'
     }
   },
   methods: {
@@ -41,7 +73,7 @@ color: red;
       style.type = "text/css"
       style.appendChild(document.createTextNode(''))
       this.styleNode = style.childNodes[0] // a reference I store in the data hash
-      style.innerHTML = '.' + this.classAttr + this.styles;
+      style.innerHTML = this.styles;
       document.head.appendChild(style)
 
     }
@@ -52,12 +84,20 @@ color: red;
 <style>
   h2{
     text-align: center;
+    font-size: 24px;
+  }
 
+  .clearfix:after{
+    content: ' ';
+    height: 0;
+    visibility: none;
+    clear: both;
   }
   textarea, input{
     padding: 10px;
     font-family: 'Courier New', Courier, monospace;
     border: none;
+    height: 93vh;
   }
   label{
     font-weight: bold;
@@ -66,15 +106,38 @@ color: red;
   }
   textarea{
     background: #eee;
-    width: 98%;
     box-shadow: 0px 0px 1px black;
     line-height: 14pt;
+    width: 28%;
+    position: fixed;
   }
   input{
     padding: 10px;
     margin: 20px 0;
     border-bottom: thin solid black;
   }
-  label{
+  .viewport{
+    width: 100%;
+  }
+  .align-left{
+    float: left;
+    width: 30%;
+  }
+  .align-right{
+    float: right;
+    width: auto;
+    text-align: center;
+    width: 70%;
+    height: 98vh;
+  }
+  section, figure{
+    border: thin solid black;
+    padding: 10px;
+    margin: 10px;
+
+  }
+  ul li{
+    list-style-type: none;
+    line-height: 18pt;
   }
 </style>
